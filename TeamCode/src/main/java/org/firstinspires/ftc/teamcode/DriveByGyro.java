862772dfd6857b7635cc2e2827fbd83de07b361b
Unknown <aidanb1049@gmail.com>
2018-07-01 17:21:04 -0700

    Added skeleton for DriveByGyro

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveByGyro.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveByGyro.java
@@ -2,4 +2,24 @@ package org.firstinspires.ftc.teamcode;
 
 //TODO
 public class DriveByGyro {
+
+    public void rotateToAngle(double angle, double power){
+
+    }
+
+    public double getAngle(){
+
+    }
+
+    public double getPitch(){
+
+    }
+
+    public double getYaw(){
+
+    }
+
+    public double getRoll(){
+
+    }
 }
