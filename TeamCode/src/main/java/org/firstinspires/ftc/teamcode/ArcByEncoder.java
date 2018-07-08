3a24ad27aee8d00b20b39e4c5c596b5e2025f1a5
Unknown <summithighschoolrobotics@gmail.com>
2018-07-07 21:07:50 -0700

    idk man commit this 2: electric boogaloo

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ArcByEncoder.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ArcByEncoder.java
@@ -2,12 +2,7 @@ package org.firstinspires.ftc.teamcode;
 
 public class ArcByEncoder {
 
-    public static void Main(String args[]){
-        System.out.println(getLengthOfArc(3,3));
-
-    }
-
-    public static double getLengthOfArc(double x, double y){
+    public static double PerimiterOfElipseOfTurn(double x, double y){
         return Math.PI*(3*(x+y) - Math.sqrt(((3*x+y)*(x+3*y))));
     }
 
